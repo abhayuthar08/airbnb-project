92e496385b5159a751177e8077b5c2a693cd5f9c
Abhay Suthar <abhaysuthar6355@gmail.com>
2024-07-20 16:21:07 +0530

    Revert "final phase1"
    This reverts commit a3ed8d813b77f4c6a306643e76a447f7ac03a316.

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js')
-const ExpressError = require('./utils/ExpressError.js')
-const {listingSchema} = require('./views/includes/schema.js')
 
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, 'views'));
@@ -33,26 +30,15 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-const validateListing = (req , res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    console.log(error);
-    if(error) {
-        throw new ExpressError(400, error);
-    }
-    else {
-        next();
-    }
-}
-
 app.get('/', (req, res) => {
     res.send("working");
 });
 
 // Index Route
-app.get('/listings', wrapAsync ( async (req, res) => {
+app.get('/listings', async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index", { allListings });
-}));
+});
 
 // New Route
 app.get('/listings/new', (req, res) => {
@@ -60,62 +46,42 @@ app.get('/listings/new', (req, res) => {
 });
 
 // Show Route
-app.get('/listings/:id', wrapAsync ( async (req, res) => {
+app.get('/listings/:id', async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show", { listing });
-}));
+});
 
 
 //Create Route
-app.post('/listings', validateListing , wrapAsync  (async (req, res) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400 , "Send a valid data for listing")
-    }
+app.post('/listings', async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     console.log(newListing);
     res.redirect('/listings');
-}));
+});
 
 //Edit Route
-app.get('/listings/:id/edit', wrapAsync ( async (req, res) => {
-   
+app.get('/listings/:id/edit', async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-   
-}));
+});
 
 // Update Route
-app.put('/listings/:id', validateListing ,  wrapAsync (async (req, res) => {
+app.put('/listings/:id', async (req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-}));
+});
 
 //Delete Route
-app.delete('/listings/:id' , wrapAsync (async (req, res) => {
+app.delete('/listings/:id' , async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing); 
         res.redirect('/listings'); 
-}));
-
-//Error handling
-// app.use((err,req,res,next) =>  {
-//     res.send("something went wrong");
-// });
-
-
-app.all('*', (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found"));
-});
-
-app.use((err,req,res,next) => {
-    let { statusCode=500, message="something went wrong?"} = err;
-    res.status(statusCode).render("error.ejs" , {message});
-});
+})
 
 
 
--- models/listing.js
+++ models/listing.js
@@ -9,7 +9,7 @@ const listingSchema = new Schema({
     description: String,
     image: {
         type: String,
-        default: "https://plus.unsplash.com/premium_photo-1673002094103-b2657755f800?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        default: "https://plus.unsplash.com/premium_photo-1684470563670-a11e9fc64a53?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: v => v === "" ? "https://plus.unsplash.com/premium_photo-1684470563670-a11e9fc64a53?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v
     },
     price: Number,
